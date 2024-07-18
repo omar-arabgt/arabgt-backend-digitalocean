168a95c5c2eda30c7019c7190d95194a4ac192ef
zeyadzaheraudteye <zeyad@audteye.com>
2024-07-18 09:57:37 +0300

    add title and tag in list

--- api/views.py
+++ api/views.py
@@ -1,14 +1,18 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, PostListSerializer
 from .filters import PostFilter 
 
 class PostView(GenericAPIView, ListModelMixin, RetrieveModelMixin):
-    serializer_class = PostSerializer
     queryset = Post.objects.all()
     filterset_class = PostFilter
     lookup_field = 'id'
+
+    def get_serializer_class(self):
+        if 'id' not in self.kwargs:
+            return PostListSerializer
+        return PostSerializer
 
     def get(self, request, *args, **kwargs):
         if 'id' in kwargs:
