a1e9dae8133b36a4f838cd1cd7f101386d33351b
zeyadzaheraudteye <zeyad@audteye.com>
2024-07-23 16:09:23 +0300

    logout functionality

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import *
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="web-login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("home/", HomeView.as_view(), name="home"),
 ]
