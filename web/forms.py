00b8e68cd50b2e682431e7731ed0944236d4e3fc
zeyadzaheraudteye <zeyad@audteye.com>
2024-07-23 15:32:25 +0300

    - add pagination partial - add search partial - add fav icon - fix select input - fix date input - fix page titles

--- web/forms.py
+++ web/forms.py
@@ -1,54 +1,33 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django.core.exceptions import ValidationError
-
-from api.models import *
-
-class CustomFormMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400'
-            })
-class UserForm(CustomFormMixin, forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'nick_name', 'phone_number', 'birth_date', 'gender', 'nationality', 'country', 'has_business', 'has_car', 'car_type', 'hobbies', 'favorite_presenter', 'favorite_show']
-        labels = {
-            'username': 'اسم المستخدم',
-            'email': 'البريد الإلكتروني',
-            'nick_name': 'اللقب',
-            'phone_number': 'رقم الهاتف',
-            'birth_date': 'تاريخ الميلاد',
-            'gender': 'الجنس',
-            'nationality': 'الجنسية',
-            'country': 'البلد',
-            'has_business': 'لديه عمل',
-            'has_car': 'لديه سيارة',
-            'car_type': 'نوع السيارة',
-            'hobbies': 'هوايات',
-            'favorite_presenter': 'المذيع المفضل',
-            'favorite_show': 'العرض المفضل',
-        }
-        help_texts = {
-            'username': 'مطلوب. 150 حرف أو أقل. حروف، أرقام و @/./+/-/_ فقط.',
-        }
-
-
-from api.models import *
+from api.models import User
 
 class CustomFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400'
-            })
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs.update({
+                    'class': 'custom-select w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 appearance-none bg-white'
+                })
+            elif isinstance(field.widget, forms.DateInput):
+                field.widget.attrs.update({
+                    'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400',
+                    'type': 'date'
+                })
+            else:
+                field.widget.attrs.update({
+                    'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400'
+                })
+
 class UserForm(CustomFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'nick_name', 'phone_number', 'birth_date', 'gender', 'nationality', 'country', 'has_business', 'has_car', 'car_type', 'hobbies', 'favorite_presenter', 'favorite_show']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+        }
         labels = {
             'username': 'اسم المستخدم',
             'email': 'البريد الإلكتروني',
--- web/views.py
+++ web/views.py
@@ -1,17 +1,29 @@
 from django.contrib.auth import views as auth_views
-from django.views.generic import ListView, UpdateView
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView, UpdateView, TemplateView
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from api.models import User
+from django.db.models import Q
 from .forms import *
 
 class UserListView(LoginRequiredMixin, ListView):
     model = User
     template_name = 'web/users/list.html'
     context_object_name = 'users'
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return User.objects.filter(Q(username__icontains=query))
+        return User.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_name'] = 'قائمة المستخدمين'
+        context['search_query'] = self.request.GET.get('q', '')
+        return context
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
@@ -22,11 +34,24 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     def get_object(self):
         return get_object_or_404(User, pk=self.kwargs['pk'])
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_name'] = 'تعديل المستخدم'
+        return context
 
 class LoginView(auth_views.LoginView):
     template_name = "web/login.html"
     form_class = CustomAuthenticationForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_name'] = 'تسجيل الدخول'
+        return context
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "web/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_name'] = 'لوحة التحكم'
+        return context
