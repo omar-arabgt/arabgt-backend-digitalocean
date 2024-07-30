3643d02757851cdf2b6b2a4b6695ec4047e48d77
zeyadzaheraudteye <zeyad@audteye.com>
2024-07-30 09:39:47 +0300

    dashboard-delete

--- web/urls.py
+++ web/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("home/", HomeView.as_view(), name="home"),
     path('users/', UserListView.as_view(), name='user_list'),
     path('users/edit/<int:pk>/', UserUpdateView.as_view(), name='user_edit'),
+    path('deleted_user/', DeletedUserListView.as_view(), name='deleted_user_list'),
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
     path('download_newsletter_excel/', download_newsletter_excel, name='download_newsletter_excel'),
     path('newsletter/', NewsletterListView.as_view(), name='newsletter_list'),
 ]
