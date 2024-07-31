f2ec047f4939c1ee0a80e816549b6e2a7776d6e7
mehmetcangms <mehmet.gumus@chooch.com>
2024-07-31 10:26:24 +0300

    added rate limiting

--- core/settings.py
+++ core/settings.py
@@ -146,6 +146,14 @@ AUTH_USER_MODEL = "api.User"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 SIMPLE_JWT = {
