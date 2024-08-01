efcd46bc9c8f3d55e1cafc886a1e7cf7e373ba98
zeyadzaheraudteye <zeyad@audteye.com>
2024-08-01 18:07:46 +0300

    groups

--- web/forms.py
+++ web/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django.core.exceptions import ValidationError
-from api.models import User
+from api.models import User, Group
 
 class CustomFormMixin:
     def __init__(self, *args, **kwargs):
@@ -74,3 +74,29 @@ class CustomAuthenticationForm(AuthenticationForm):
                 self.error_messages["invalid_login"],
                 code="invalid_login",
             )
+
+class ForumForm(CustomFormMixin, forms.ModelForm):
+    class Meta:
+        model = Forum
+        fields = ['name', 'image']
+        labels = {
+            'name': 'اسم المجموعة',
+            'image': 'صورة',   
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['is_active'] = forms.BooleanField(
+                label='مفعل؟',
+                initial=self.instance.is_active,
+                required=False,
+            )
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if not self.instance.pk:
+            instance.is_active = True
+        if commit:
+            instance.save()
+        return instance
--- web/urls.py
+++ web/urls.py
@@ -13,6 +13,12 @@ urlpatterns = [
     path('export_users/', ExportUserListView.as_view(), name='export_user_list'),
     path('export_users_excel/', ExportUserToExcelView.as_view(), name='export_users_excel'),
     path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+    
+    path('forums/', ForumListView.as_view(), name='forum_list'),
+    path('forums/create', ForumCreateView.as_view(), name='forum_create'),
+    path('forums/<int:pk>/delete/', ForumUpdateView.as_view(), name='forum_edit'),
+
+    path('newsletter/', NewsletterListView.as_view(), name='newsletter_list'),
     path('download_newsletter_excel/', download_newsletter_excel, name='download_newsletter_excel'),
     path('newsletter/', NewsletterListView.as_view(), name='newsletter_list'),
 ]
