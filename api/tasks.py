fcc8902d3b71cd290a7428dbd3bf63787ad9b71f
mehmetcangms <mehmet.gumus@chooch.com>
2024-09-02 10:01:03 +0300

    added send_notification field

--- api/tasks.py
+++ api/tasks.py
@@ -18,40 +18,39 @@ def send_push_notification(user_id, title, content, link=None):
     from .models import User, Notification
 
     if user_id == NOTIFICATION_ALL:
-        user_info = {"included_segments": ["All"]}
+        users = User.objects.filter(send_notification=True)
     else:
-        user_info = {"include_external_user_ids": [str(user_id)]}
+        users = User.objects.filter(id=user_id, send_notification=True)
 
-    configuration = Configuration(app_key=settings.ONESIGNAL_API_KEY)
-    with ApiClient(configuration) as api_client:
-        api_instance = default_api.DefaultApi(api_client)
-        notification = OneSignalNotification(
-            app_id=settings.ONESIGNAL_APP_ID,
-            contents={"en": content},
-            headings={"en": title},
-            url=link,
-            channel_for_external_user_ids="push",
-            **user_info,
-        )
-        api_instance.create_notification(notification)
+    if users:
+        user_ids = users.values_list("id", flat=True)
+        user_ids = [str(i) for i in user_ids]
 
-    notifications = []
-    if user_id == NOTIFICATION_ALL:
-        users = User.objects.all()
-    else:
-        users = User.objects.filter(id=user_id)
+        configuration = Configuration(app_key=settings.ONESIGNAL_API_KEY)
+        with ApiClient(configuration) as api_client:
+            api_instance = default_api.DefaultApi(api_client)
+            notification = OneSignalNotification(
+                app_id=settings.ONESIGNAL_APP_ID,
+                include_external_user_ids=user_ids,
+                contents={"en": content},
+                headings={"en": title},
+                url=link,
+                channel_for_external_user_ids="push",
+            )
+            api_instance.create_notification(notification)
 
-    for user in users:
-        n = Notification(
-            user=user,
-            title=title,
-            content=content,
-            link=link,
-            is_admin_notification=True,
-        )
+        notifications = []
+        for user in users:
+            n = Notification(
+                user=user,
+                title=title,
+                content=content,
+                link=link,
+                is_admin_notification=True,
+            )
         notifications.append(n)
 
-    Notification.objects.bulk_create(notifications)
+        Notification.objects.bulk_create(notifications)
 
 
 @shared_task
