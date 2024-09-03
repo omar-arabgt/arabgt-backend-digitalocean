057115b06df6c387332bf755eb2a55b916fe95da
zeyadzaheraudteye <zeyad@audteye.com>
2024-09-03 16:44:05 +0300

    .

--- api/migrations/0024_alter_favoritepresenter_image_and_more.py
+++ api/migrations/0024_alter_favoritepresenter_image_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0022_groupmembership_is_active'),
+        ('api', '0023_user_profile_photo'),
     ]
 
     operations = [
